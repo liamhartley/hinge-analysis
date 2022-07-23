6d3d1b1fe1741546e3ef3257013acd0bfd20cc88
liamhartley <liamjhartley@yahoo.co.uk>
2022-07-23 09:17:43 +0100

    adding docstrings and day of matching

--- data_reader.py
+++ data_reader.py
@@ -4,8 +4,13 @@ from datetime import datetime, timedelta
 
 
 class HingeData:
-    def __init__(self, json_path: str, data_type: str):
 
+    def __init__(self, json_path: str, data_type: str):
+        '''
+        Base class object to analyse your Hinge data
+        :param json_path: the path to your file to analyse
+        :param data_type: all Hinge filetypes that are available for analysis
+        '''
         data_types = ['matches']
         assert data_type in data_types
 
@@ -17,7 +22,25 @@ class HingeData:
         with open(self.json_path, "r") as read_file:
             self.json_dict = json.load(read_file)
 
+    def get_message_time(self, message, field):
+        '''
+        Used to handle different datetime formats in Hinge data
+        :param message:
+        :param field:
+        :return:
+        '''
+        try:
+            message_time = datetime.fromtimestamp(
+                time.mktime(time.strptime(message[field], self.datetime_format)))
+        except ValueError:
+            message_time = datetime.fromtimestamp(
+                time.mktime(time.strptime(message[field], self.datetime_format_alternative)))
+        return message_time
+
     def average_response_time(self):
+        '''
+        Average time that you take to respond to messages
+        '''
         average_response_times = []
         for interaction in self.json_dict:
             total_response_time = timedelta(0)
@@ -25,18 +48,8 @@ class HingeData:
                 if len(interaction['chats']) > 1:
                     previous_message = interaction['chats'][0]
                     for message in interaction['chats'][1:]:
-                        try:
-                            first_message_time = datetime.fromtimestamp(
-                                time.mktime(time.strptime(previous_message['timestamp'], self.datetime_format)))
-                        except ValueError:
-                            first_message_time = datetime.fromtimestamp(
-                                time.mktime(time.strptime(previous_message['timestamp'], self.datetime_format_alternative)))
-                        try:
-                            second_message_time = datetime.fromtimestamp(
-                                time.mktime(time.strptime(message['timestamp'], self.datetime_format)))
-                        except ValueError:
-                            second_message_time = datetime.fromtimestamp(
-                                time.mktime(time.strptime(message['timestamp'], self.datetime_format_alternative)))
+                        first_message_time = self.get_message_time(previous_message, 'timestamp')
+                        second_message_time = self.get_message_time(message, 'timestamp')
                         response_time = second_message_time - first_message_time
                         total_response_time = response_time + total_response_time
                         previous_message = message
@@ -50,24 +63,24 @@ class HingeData:
         print(f"slowest response time: {max(average_response_times)}")
         print(f"fastest response time: {min(average_response_times)}")
 
-    def like_conversion_rate(self):
+    def like_statistics(self):
+        '''
+        Prints statistics about matches, likes sent, like conversion and unmatching
+        :return:
+        '''
         like_no_match = 0
         matches = 0
+        declined = 0
+        unmatches = 0
         for interaction in self.json_dict:
             if 'like' in interaction:
                 if 'match' in interaction:
                     matches += 1
                 else:
                     like_no_match += 1
-
-        unmatches = 0
-        for interaction in self.json_dict:
             if 'like' in interaction:
                 if 'block' in interaction:
                     unmatches += 1
-
-        declined = 0
-        for interaction in self.json_dict:
             if 'block' in interaction:
                 if 'match' not in interaction:
                     declined += 1
@@ -80,20 +93,14 @@ class HingeData:
         print(f"total number of people I have unmatched: {unmatches}")
         print(f"total number of people I have declined: {declined}")
 
-    def get_match_time(self, interaction):
-        try:
-            match_time = datetime.fromtimestamp(
-                time.mktime(time.strptime(interaction['match'][0]['timestamp'], self.datetime_format)))
-        except ValueError:
-            match_time = datetime.fromtimestamp(
-                time.mktime(time.strptime(interaction['match'][0]['timestamp'], self.datetime_format_alternative)))
-        return match_time
-
     def my_average_match_time(self):
+        '''
+        The average time that you accept matches
+        '''
         match_times = []
         for interaction in self.json_dict:
             if 'match' in interaction:
-                match_time = self.get_match_time(interaction)
+                match_time = self.get_message_time(interaction['match'][0], 'timestamp')
                 match_times.append(match_time)
         sum_of_time = sum(map(datetime.timestamp, match_times))
         unix_average_time = datetime.fromtimestamp(sum_of_time/len(match_times))
@@ -102,19 +109,52 @@ class HingeData:
         print(f"average match time for likes received: {average_time}")
 
     def their_average_match_time(self):
+        '''
+        The average time that your likes are accepted
+        '''
         match_times = []
         for interaction in self.json_dict:
             if 'match' in interaction and 'like' in interaction:
-                match_time = self.get_match_time(interaction)
+                match_time = self.get_message_time(interaction['match'][0], 'timestamp')
                 match_times.append(match_time)
         sum_of_time = sum(map(datetime.timestamp, match_times))
         unix_average_time = datetime.fromtimestamp(sum_of_time/len(match_times))
         average_time = datetime.strftime(unix_average_time, "%H:%M:%S")
 
-        print(f"average match time for likes sent: {average_time}")
+        print(f"average match time for likes accepted: {average_time}")
 
+    def average_match_day_theirs(self):
+        match_days = []
+        for interaction in self.json_dict:
+            if 'match' in interaction and 'like' in interaction:
+                match_time = self.get_message_time(interaction['match'][0], 'timestamp')
+                match_day = match_time.isoweekday()
+                match_days.append(match_day)
 
+        most_matches = 0
+        most_matched_day = 0
+        for match_day in [1, 2, 3, 4, 5, 6, 7]:
+            matches_on_day = match_days.count(match_day)
+            if matches_on_day > most_matches:
+                most_matches = matches_on_day
+                most_matched_day = match_day
 
+        print(f"most common day for your likes to be accepted: {most_matched_day} ({most_matches})")
 
+    def average_match_day_yours(self):
+        match_days = []
+        for interaction in self.json_dict:
+            if 'match' in interaction:
+                match_time = self.get_message_time(interaction['match'][0], 'timestamp')
+                match_day = match_time.isoweekday()
+                match_days.append(match_day)
 
+        most_matches = 0
+        most_matched_day = 0
+        for match_day in [1, 2, 3, 4, 5, 6, 7]:
+            matches_on_day = match_days.count(match_day)
+            if matches_on_day > most_matches:
+                most_matches = matches_on_day
+                most_matched_day = match_day
 
+        print(f"most common day for you to accept likes: {most_matched_day} ({most_matches})")
--- main.py
+++ main.py
@@ -3,16 +3,19 @@ from data_reader import HingeData
 
 if __name__ == '__main__':
 
-    data_path = '/Users/liamhartley/PycharmProjects/hinge-analysis/export/matches.json'
+    data_path = '/Users/liam.hartley/PycharmProjects/hinge-analysis/export/matches.json'
 
     HingeDataObject = HingeData(json_path=data_path, data_type='matches')
     HingeDataObject.average_response_time()
-    HingeDataObject.like_conversion_rate()
+    HingeDataObject.like_statistics()
     HingeDataObject.my_average_match_time()
     HingeDataObject.their_average_match_time()
+    HingeDataObject.average_match_day_yours()
+    HingeDataObject.average_match_day_theirs()
 
 # TODO
 # Transform the matches.json to amend all dates to one format
 # add export to .gitignore
 # push
 # README
+# SCALE this solution for large amount of files and write to a new .csv file
